7fd9385c5469034d360b844bf61b5ec85872ae8a
Alois Sečkár <ellrohir@seznam.cz>
2015-02-02 00:37:32 +0100

    Initial GIT commit

---
+++ src/db/DBHandler.java
@@ -0,0 +1,169 @@
+// Hibernate DB handling methods for VSEGRAF project
+// 
+// @author Alois Seckar [ ellrohir@seznam.cz ]
+// @version 0.1
+//
+// Last modified: 2015-02-01 1658 GMT by Alois Seckar
+
+package db;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * DBHandler - provides bunch of static methods to handle the Hibernate
+ * routines.
+ *
+ * @author Alois Seckar
+ * @version 1.0
+ */
+public class DBHandler {
+
+    // ************************** \\
+    // *        CONSTANTS       * \\
+    // ************************** \\
+
+    // ************************** \\
+    // *       PROPERTIES       * \\
+    // ************************** \\
+
+    // ************************** \\
+    // *      CONSTRUCTORS      * \\
+    // ************************** \\
+
+    // ************************** \\
+    // *     ACCESS METHODS     * \\
+    // ************************** \\
+
+    // ************************** \\
+    // *     PUBLIC METHODS     * \\
+    // ************************** \\
+    
+    public static Object getSingleObject(String query) {
+        Object result = new Object();
+        try {
+            Session session = createSession();
+            try {
+                session.getTransaction().begin();
+                result = session.createQuery(query).uniqueResult();
+                session.getTransaction().commit();
+            } catch (RuntimeException e) {
+                session.getTransaction().rollback();
+                // TODO log error
+            }
+            closeSession(session);
+        } catch (HibernateException e) {
+            // TODO log error
+        }
+        //
+        return result;
+    }
+    
+    public static Iterator getListOfObjects(String query) {
+        List result = new ArrayList();
+        try {
+            Session session = createSession();
+            try {
+                session.getTransaction().begin();
+                result = session.createQuery(query).list();
+                session.getTransaction().commit();
+            } catch (RuntimeException e) {
+                session.getTransaction().rollback();
+                // TODO log error
+            }
+            closeSession(session);
+        } catch (HibernateException e) {
+            // TODO log error
+        }
+        //
+        return result.iterator();
+    }
+    
+    public static String saveObject(Object object) {
+        try {
+            Session session = createSession();
+            try {
+                session.getTransaction().begin();
+                session.save(object);
+                session.getTransaction().commit();
+            } catch (RuntimeException e) {
+                session.getTransaction().rollback();
+                // TODO log error
+            }
+            //
+            return "S_SUCCESS";
+        } catch (HibernateException e) {
+            // TODO log error
+            return "S_ERROR " + e.getMessage();
+        }
+    }
+    
+    public static String updateObject(Object object) {
+        try {
+            Session session = createSession();
+            try {
+                session.getTransaction().begin();
+                session.update(object);
+                session.getTransaction().commit();
+            } catch (RuntimeException e) {
+                session.getTransaction().rollback();
+                // TODO log error
+            }
+            //
+            return "S_SUCCESS";
+        } catch (HibernateException e) {
+            // TODO log error
+            return "S_ERROR " + e.getMessage();
+        }
+    }
+    
+    public static String deleteObject(Object object) {
+        try {
+            Session session = createSession();
+            try {
+                session.getTransaction().begin();
+                session.delete(object);
+                session.getTransaction().commit();
+            } catch (RuntimeException e) {
+                session.getTransaction().rollback();
+                // TODO log error
+            }
+            //
+            return "S_SUCCESS";
+        } catch (HibernateException e) {
+            // TODO log error
+            return "S_ERROR " + e.getMessage();
+        }
+    }
+
+    // ************************** \\
+    // *    PRIVATE METHODS     * \\
+    // ************************** \\
+    
+    private static Session createSession() {
+        // create session
+        Configuration configuration = new Configuration().configure();
+        StandardServiceRegistryBuilder builder = 
+                new StandardServiceRegistryBuilder().
+                        applySettings(configuration.getProperties());
+        SessionFactory factory = 
+                configuration.buildSessionFactory(builder.build());
+        Session session = factory.openSession();
+        // return prepared session
+        return session;
+    }
+    
+    private static void closeSession(Session session) {
+        session.getTransaction().commit(); 
+        // dismiss session    
+        session.flush();
+        session.close();
+    }
+
+}
---
+++ src/db/KBEntry.java
@@ -0,0 +1,131 @@
+// edit: 2015-01-31 21:09
+// by:   AS
+
+package db;
+
+/**
+ * KBEntry - Represents one "page" in knowledge base
+ * 
+ * @author Alois Seckar [ ellrohir@seznam.cz ]
+ * @version 0.1
+ */
+public class KBEntry {
+
+    // ************************** \\
+    // *        CONSTANTS       * \\
+    // ************************** \\
+
+    // ************************** \\
+    // *       PROPERTIES       * \\
+    // ************************** \\
+    
+    /**
+     * Internal DB ID.
+     */
+    private int kbID;
+    
+    /**
+     * Page title to be displayed.
+     */
+    private String entryTitle;
+    
+    /**
+     * Page category for listing.
+     */
+    private String entryCat;
+    
+    /**
+     * Page contents.
+     * Text with possibilty to use supported tags for formatting and linking.
+     */
+    private String entryBody;
+
+    // ************************** \\
+    // *      CONSTRUCTORS      * \\
+    // ************************** \\
+    
+    // empty constructor required by Hibernate
+    
+    public KBEntry() {
+    }
+    
+    // standard constructor
+    
+    public KBEntry(int kbID, String eTitle, String eCat, String eBody) {
+        this.kbID = kbID;
+        this.entryTitle = eTitle;
+        this.entryCat = eCat;
+        this.entryBody = eBody;
+    }
+
+    // ************************** \\
+    // *     ACCESS METHODS     * \\
+    // ************************** \\
+    
+    // getters and setters required for Hibernate
+
+    public int getKbID() {
+        return kbID;
+    }
+
+    public void setKbID(int kbID) {
+        this.kbID = kbID;
+    }
+
+    public String getEntryTitle() {
+        return entryTitle;
+    }
+
+    public void setEntryTitle(String entryTitle) {
+        this.entryTitle = entryTitle;
+    }
+
+    public String getEntryCat() {
+        return entryCat;
+    }
+
+    public void setEntryCat(String entryCat) {
+        this.entryCat = entryCat;
+    }
+
+    public String getEntryBody() {
+        return entryBody;
+    }
+
+    public void setEntryBody(String entryBody) {
+        this.entryBody = entryBody;
+    }
+
+    // ************************** \\
+    // *     PUBLIC METHODS     * \\
+    // ************************** \\
+    
+    // overrides required for Hibernate
+    
+    @Override
+    public int hashCode() {
+        final int prime = 2108; // coded at "21:08"
+        int result = 1;
+        result = prime * result + Integer.valueOf(kbID).hashCode();
+        result = prime * result + Integer.valueOf(entryTitle).hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof KBEntry)) {
+            return false;
+        }
+        return this == obj;
+    }
+
+    @Override
+    public String toString() {
+        return entryTitle;
+    }
+
+    // ************************** \\
+    // *    PRIVATE METHODS     * \\
+    // ************************** \\
+
+}
